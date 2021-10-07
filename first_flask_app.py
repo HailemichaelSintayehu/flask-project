aa8848100e0cec8ae6b0b0b8a430f1b4ea572c54
Michael <hmichaelsintayehu@gmail.com>
2021-10-07 21:27:02 +0300

    redirect,url_for

--- first_flask_app.py
+++ first_flask_app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello! this is the main page<h1>Hello</>"
-
+@app.route("/<name>")
+def user(name):
+    return f"hello {name}!"
+@app.route("/admin/")
+def admin():
+    return redirect(url_for('user',name="Admin!"))
 
 if __name__=='__main__':
     app.run()
+
