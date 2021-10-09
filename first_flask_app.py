1bc63b645d630d9f2070be994355153117465ae3
Michael <hmichaelsintayehu@gmail.com>
2021-10-09 13:08:00 +0300

    sixth commit,inheritance

--- first_flask_app.py
+++ first_flask_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html",content={'time':'zara','joe':7,'bill':'first'})
+    return render_template("index.html")
 
 if __name__=='__main__':
     app.run(debug=True)
